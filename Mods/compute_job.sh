eaca131ea0cc234d00e7beec9344c10021f7ae36
MendezV <som.felipe66@gmail.com>
2022-01-25 18:51:05 -0500

    compute run

---
+++ Mods/compute_job.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=ALFsharedtrial
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jfm343@cornell.edu
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --mem=124G
+#SBATCH -t 48:00:00
+#SBATCH --account=crl171
+#SBATCH --output="job.%j.%N.out"
+#SBATCH --error="job.%j.%N.err"
+#SBATCH --export=ALL
+
+
+module purge
+module load cpu
+
+#Load module file(s) into the shell environment
+module load gcc openblas openmpi hdf5
+module load slurm
+module load matlab
+module load anaconda3
+
+#running
+srun --mpi=pmi2 -n 128 TBG_Landau.out
+
+##moving data out
+date_fin="`date "+%Y-%m-%d-%H-%M-%S"`"
+dirfin="${PRDIR}/${SLURM_JOB_NAME}_${date_fin}"
+mkdir "${dirfin}"
+mv * "${dirfin}"
