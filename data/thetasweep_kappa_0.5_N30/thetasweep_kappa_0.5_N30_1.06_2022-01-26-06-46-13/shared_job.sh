0792cc3e59ca2bc57487a8a20654bb6fba76223c
Juan Mendez <som.felipe66@gmail.com>
2022-01-26 14:08:59 -0500

    data at different kappa

---
+++ data/thetasweep_kappa_0.5_N30/thetasweep_kappa_0.5_N30_1.06_2022-01-26-06-46-13/shared_job.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=ALFsharedtrial
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jfm343@cornell.edu
+#SBATCH --partition=shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=124G
+#SBATCH -t 48:00:00
+#SBATCH --account=crl171
+#SBATCH --output="job.%j.%N.out"
+#SBATCH --error="job.%j.%N.err"
+#SBATCH --export=ALL
+
+
+module purge
+module load cpu
+
+#Load module file(s) into the shell environment
+module load gcc openblas openmpi hdf5
+module load slurm
+module load matlab
+module load anaconda3
+
+
+#running
+srun python -u Bubble_ep.py 0 30 L ${param_val} 1
+
+
+##moving data out
+date_fin="`date "+%Y-%m-%d-%H-%M-%S"`"
+dirfin="${PRDIR}/${SLURM_JOB_NAME}_${date_fin}"
+mkdir "${dirfin}"
+mv * "${dirfin}"
