dc4f11f03e0fd606d0fddefb6e296c5ef25d4548
MendezV <som.felipe66@gmail.com>
2022-07-04 22:53:20 -0400

    casdfasdf

--- run_scripts/run_param.sh
+++ run_scripts/run_param.sh
@@ -13,7 +13,7 @@ Lattice_size=30
 filling_seed=0
 twist_angle=1.05
 kappa=0.7
-Mode_HF=0
+Mode_HF=1
 phonon_polarization='T'
 
 #needed prerequisites for the run
@@ -25,7 +25,7 @@ pow=$PWD
 param_arr=$(awk -F= '{print $1}' ${parameter_file})
 echo ${param_arr}
 
-jobname="thetasweep_betasweep"  #JOBNAME importan to declare -has to be descriptive
+jobname="thetasweep_betasweep_k07_HF"  #JOBNAME importan to declare -has to be descriptive
 
 #General info about the job
 date_in="`date "+%Y-%m-%d-%H-%M-%S"`"
