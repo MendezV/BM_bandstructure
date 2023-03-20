cdf67432cef2c5aa334d88523f450dc01d0d3e5a
Felipe <som.felipe66@gmail.com>
2023-03-19 20:43:32 -0400

    all data for one order param at a atime

--- Mods/Mean_field_M.py
+++ Mods/Mean_field_M.py
@@ -198,7 +198,7 @@ class ep_Bubble:
                 [self.Omega_FFp,self.Omega_FFm]=self.HB.Form_factor_unitary(self.HB.FFp.NemqFFT_a(), self.HB.FFm.NemqFFT_a())
             elif mode=="Mins":
                 [self.Omega_FFp,self.Omega_FFm]=self.HB.Form_factor_unitary(self.HB.FFp.NemqFFT_a(), self.HB.FFm.NemqFFT_a())
-                # [self.sublp,self.sublm]=self.HB.Form_factor_unitary(self.HB.FFp.sublFF_s(), self.HB.FFm.sublFF_s())
+                [self.sublp,self.sublm]=self.HB.Form_factor_unitary(self.HB.FFp.sublFF_s(), self.HB.FFm.sublFF_s())
             elif mode=="dens":
                 [self.Omega_FFp,self.Omega_FFm]=self.HB.Form_factor_unitary(self.HB.FFp.denFF_s(), self.HB.FFm.denFF_s())
             elif mode=="subl":
@@ -313,8 +313,7 @@ class ep_Bubble:
         
             Lp1=self.Omega_FFp[ik, ikq,:,:]+self.Omega_FFp[ik, ikmq,:,:]
             Lm1=-(sx@self.Omega_FFp[ik, ikq,:,:]@sx) -(sx@self.Omega_FFp[ik, ikmq,:,:]@sx)#using chiral
-            # Lp1=Lp1*0
-            # Lm1=Lm1*0
+            
             
             print("chiral ph",np.mean(np.abs(Lp1[:,:]+sx@Lm1[:,:]@sx)))
             print("chiral ph",np.mean(np.abs(Lm1[:,:]+sx@Lp1[:,:]@sx)))
@@ -323,18 +322,23 @@ class ep_Bubble:
             print("c2T ",np.mean(np.abs(Lp1[:,:]-sz@np.conj(Lp1[:,:])@sz)))
             print("c2T ",np.mean(np.abs(Lm1[:,:]-sz@np.conj(Lm1[:,:])@sz)))
             
-            # Sp1 = self.sublp[ik, ikq2,:,:]+self.sublp[ik, ikmq2,:,:]
-            # Sm1 = -(sx@self.sublp[ik, ikq2,:,:]@sx) - (sx@self.sublp[ik, ikmq2,:,:]@sx)#using chiral
+            Sp1 = self.sublp[ik, ikq2,:,:]+self.sublp[ik, ikmq2,:,:]
+            Sm1 = -(sx@self.sublp[ik, ikq2,:,:]@sx) - (sx@self.sublp[ik, ikmq2,:,:]@sx)#using chiral
+            
+            
+            print("subl chiral ph",np.mean(np.abs(Sp1[:,:]+sx@Sm1[:,:]@sx)))
+            print("subl chiral ph",np.mean(np.abs(Sm1[:,:]+sx@Sp1[:,:]@sx)))
+            print("subl chiral sub",np.mean(np.abs(Sp1[:,:]-sz@Sp1[:,:]@sz)))
+            print("subl chiral sub",np.mean(np.abs(Sm1[:,:]-sz@Sm1[:,:]@sz)))
+            print("subl c2T ",np.mean(np.abs(Sp1[:,:]+sz@np.conj(Sp1[:,:])@sz)))
+            print("subl c2T ",np.mean(np.abs(Sm1[:,:]+sz@np.conj(Sm1[:,:])@sz)))
+
+            
+            # Lp1=Lp1*0
+            # Lm1=Lm1*0
+            
             # Sp1=Sp1*0
             # Sm1=Sm1*0
-            
-            # print("subl chiral ph",np.mean(np.abs(Sp1[:,:]+sx@Sm1[:,:]@sx)))
-            # print("subl chiral ph",np.mean(np.abs(Sm1[:,:]+sx@Sp1[:,:]@sx)))
-            # print("subl chiral sub",np.mean(np.abs(Sp1[:,:]-sz@Sp1[:,:]@sz)))
-            # print("subl chiral sub",np.mean(np.abs(Sm1[:,:]-sz@Sm1[:,:]@sz)))
-            # print("subl c2T ",np.mean(np.abs(Sp1[:,:]+sz@np.conj(Sp1[:,:])@sz)))
-            # print("subl c2T ",np.mean(np.abs(Sm1[:,:]+sz@np.conj(Sm1[:,:])@sz)))
-
             
             
             for nband in range(self.nbands):
@@ -347,11 +351,11 @@ class ep_Bubble:
                     
                 for mband in range(self.nbands):
                     
-                    Hqp[self.nbands+nband,mband]=Hqp[self.nbands+nband,mband]+Lp1[nband,mband]#+Sp1[nband,mband]
-                    Hqp[nband,self.nbands+mband]=Hqp[nband,self.nbands+mband]+np.conj(Lp1.T)[nband,mband]#+np.conj(Sp1.T)[nband,mband]
+                    Hqp[self.nbands+nband,mband]=Hqp[self.nbands+nband,mband]+Lp1[nband,mband]+Sp1[nband,mband]
+                    Hqp[nband,self.nbands+mband]=Hqp[nband,self.nbands+mband]+np.conj(Lp1.T)[nband,mband]+np.conj(Sp1.T)[nband,mband]
 
-                    Hqm[self.nbands+nband,mband]=Hqm[self.nbands+nband,mband]+Lm1[nband,mband]#+Sm1[nband,mband]
-                    Hqm[nband,self.nbands+mband]=Hqm[nband,self.nbands+mband]+np.conj(Lm1.T)[nband,mband]#+np.conj(Sm1.T)[nband,mband]
+                    Hqm[self.nbands+nband,mband]=Hqm[self.nbands+nband,mband]+Lm1[nband,mband]+Sm1[nband,mband]
+                    Hqm[nband,self.nbands+mband]=Hqm[nband,self.nbands+mband]+np.conj(Lm1.T)[nband,mband]+np.conj(Sm1.T)[nband,mband]
 
 
 
--- run_scripts/run_MeanFieldM.sh
+++ run_scripts/run_MeanFieldM.sh
@@ -9,7 +9,7 @@
 ###########################
 
 #generic parameters 
-Lattice_size=12
+Lattice_size=24
 filling_seed=0
 twist_angle=1.05
 kappa=0.75
@@ -24,7 +24,7 @@ dire_to_mods='../Mods/'
 param_arr=$(awk -F= '{print $1}' ${parameter_file})
 echo ${param_arr}
 
-jobname="2recoverytest"  #JOBNAME importan to declare -has to be descriptive
+jobname="subl_only_M1_HR"  #JOBNAME importan to declare -has to be descriptive
 
 #General info about the job
 date_in="`date "+%Y-%m-%d-%H-%M-%S"`"
